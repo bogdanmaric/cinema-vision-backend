5cca83095503dd92a369efb0819c54c6c212632d
bogdanmaric <maricbogdan10@gmail.com>
2026-04-13 18:03:38 +0200

    feat: add validation to RegisterDto using DataAnnotations

--- MovieApi/DTOs/RegisterDto.cs
+++ MovieApi/DTOs/RegisterDto.cs
@@ -1,9 +1,17 @@
-﻿namespace MovieApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.DTOs
 {
     public class RegisterDto
     {
+        [Required]
+        [MinLength(3)]
         public string Username { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [MinLength(6)]
         public string Password { get; set; }
     }
 }
