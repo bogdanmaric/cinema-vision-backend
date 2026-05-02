4bf0f2f483e8de5612a9edaf1aca8f5a3df2c853
bogdanmaric <maricbogdan10@gmail.com>
2026-05-02 12:06:38 +0200

    test: add RegisterDto validation tests

---
+++ MovieApi.Tests/RegisterDtoTest.cs
@@ -0,0 +1,29 @@
+﻿using MovieApi.DTOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MovieApi.Tests
+{
+    public class RegisterDtoTest
+    {
+        [Fact]
+        public void Should_Fail_When_Email_Is_Missing()
+        {
+            var dto = new RegisterDto
+            {
+                Username = "testuser",
+                Password = "password123"
+                // Email is missing
+            };
+
+            var context = new ValidationContext(dto);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(dto, context, results, true);
+
+            Assert.False(isValid);
+        }
+    }
+}
