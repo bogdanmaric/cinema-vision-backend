6191c99fbd7919f3fab31a3e95ae2fb24c53aea0
bogdanmaric <maricbogdan10@gmail.com>
2026-05-05 10:21:24 +0200

    chore: ensure database schema is applied on startup using EF migrations

--- MovieApi/Program.cs
+++ MovieApi/Program.cs
@@ -69,6 +69,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+
 app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
